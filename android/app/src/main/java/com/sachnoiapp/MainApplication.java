f3823c35d8816f22cc5358ddfc49cc0a24b5a8a9
minhtcx <minhtcx@gmail.com>
2019-06-23 01:35:15 +0700

    upgrade react-native 0.59.9

--- android/app/src/main/java/com/sachnoiapp/MainApplication.java
+++ android/app/src/main/java/com/sachnoiapp/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new RNSoundPackage()
+            new RNSoundPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-/** @format */
+/**
+ * @format
+ */
 
-import {AppRegistry} from 'react-native';
-import App from './src/app';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./src/app";
+import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
